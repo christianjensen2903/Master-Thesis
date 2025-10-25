faeb816674404b22d7f4107b2cdf935c48a52be7
christianjensen2903 <cmj290300@gmail.com>
2025-10-25 11:45:37 +0200

    Revert "✨ Add graph retriever"
    This reverts commit fd53392033c5143d90117c04f9e102baf32b4303.

--- retrievers/__init__.py
+++ retrievers/__init__.py
@@ -1,13 +1,5 @@
 from .base_retriever import BaseRetriever
 from .tfidf_retriever import TfidfRetriever
 from .dense_retriever import DenseRetriever
-from .graph_retriever import GraphRetriever
-from .hybrid_retriever import HybridRetriever
 
-__all__ = [
-    "BaseRetriever",
-    "TfidfRetriever",
-    "DenseRetriever",
-    "GraphRetriever",
-    "HybridRetriever",
-]
+__all__ = ["BaseRetriever", "TfidfRetriever", "DenseRetriever"]
