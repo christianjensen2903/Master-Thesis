🐛 Fix gradient accum

gnn_trainers/gnn_trainer.py
@@ -366,15 +366,20 @@ class GNNTrainer:
             loss = loss / self.gradient_accumulation_steps
 
             # Backward pass
-            loss.backward()
+            # Retain graph for all backward calls except the last one
+            is_last_batch = batch_end >= len(anchor_ids)
+            is_accumulation_step = (
+                num_batches + 1
+            ) % self.gradient_accumulation_steps == 0
+            should_retain = not (is_last_batch or is_accumulation_step)
+
+            loss.backward(retain_graph=should_retain)
 
             total_loss += loss.item() * self.gradient_accumulation_steps
             num_batches += 1
 
             # Update weights every N accumulation steps
-            if (num_batches % self.gradient_accumulation_steps == 0) or (
-                batch_end >= len(anchor_ids)
-            ):
+            if is_accumulation_step or is_last_batch:
                 torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
                 optimizer.step()
                 optimizer.zero_grad(set_to_none=True)