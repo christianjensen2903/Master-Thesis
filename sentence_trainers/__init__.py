a38f4fcc70321c185506dc0bea01686246d31ddd
christianjensen2903 <cmj290300@gmail.com>
2025-11-03 14:54:59 +0100

    ✨ Random negative trainer

--- sentence_trainers/__init__.py
+++ sentence_trainers/__init__.py
@@ -6,6 +6,7 @@ from .base_trainer import BaseTrainer  # type: ignore
 from .base_triplet_trainer import BaseTripletTrainer  # type: ignore
 from .hard_negative_trainer import HardNegativeTrainer  # type: ignore
 from .semi_hard_trainer import SemiHardTrainer  # type: ignore
+from .random_negative_trainer import RandomNegativeTrainer  # type: ignore
 from .simcse_trainer import SentencePairTrainer  # type: ignore
 
 __all__ = [
@@ -13,5 +14,6 @@ __all__ = [
     "BaseTripletTrainer",
     "HardNegativeTrainer",
     "SemiHardTrainer",
+    "RandomNegativeTrainer",
     "SentencePairTrainer",
 ]
