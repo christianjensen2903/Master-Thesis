5d39cea97c0e5acd682027356788fa4f00756fc6
christianjensen2903 <cmj290300@gmail.com>
2025-11-05 10:13:08 +0100

    ✨ BOW retriever

--- retrievers/__init__.py
+++ retrievers/__init__.py
@@ -2,10 +2,12 @@ from .base_retriever import BaseRetriever
 from .tfidf_retriever import TfidfRetriever
 from .dense_retriever import DenseRetriever
 from .gnn_retriever import GNNRetriever
+from .bow_retriever import BOWRetriever
 
 __all__ = [
     "BaseRetriever",
     "TfidfRetriever",
     "DenseRetriever",
     "GNNRetriever",
+    "BOWRetriever",
 ]
