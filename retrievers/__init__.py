f3bcf31dc47c8c86bf8f202e7dabe976fbfc69fd
christianjensen2903 <cmj290300@gmail.com>
2025-11-06 09:17:39 +0100

    Revert "✨ ColBERT"
    This reverts commit c0011a6afc95fddc44c1f819362e4a1e33e5cca1.

--- retrievers/__init__.py
+++ retrievers/__init__.py
@@ -3,7 +3,6 @@ from .tfidf_retriever import TfidfRetriever
 from .dense_retriever import DenseRetriever
 from .gnn_retriever import GNNRetriever
 from .bow_retriever import BOWRetriever
-from .colbert_retriever import ColBERTRetriever
 
 __all__ = [
     "BaseRetriever",
@@ -11,5 +10,4 @@ __all__ = [
     "DenseRetriever",
     "GNNRetriever",
     "BOWRetriever",
-    "ColBERTRetriever",
 ]
