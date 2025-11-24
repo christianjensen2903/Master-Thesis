5c7b274b0c2344a14fedcd53d08c93a5581ba52e
christianjensen2903 <cmj290300@gmail.com>
2025-11-24 10:20:51 +0100

    🗑️ Remove unused retrievers

--- retrievers/__init__.py
+++ retrievers/__init__.py
@@ -1,15 +1,11 @@
 from .base_retriever import BaseRetriever
 from .tfidf_retriever import TfidfRetriever
 from .dense_retriever import DenseRetriever
-from .gnn_retriever import GNNRetriever
 from .bow_retriever import BOWRetriever
-from .bge_m3_retriever import BGEM3Retriever
 
 __all__ = [
     "BaseRetriever",
     "TfidfRetriever",
     "DenseRetriever",
-    "GNNRetriever",
     "BOWRetriever",
-    "BGEM3Retriever",
 ]
