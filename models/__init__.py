47aecef128afba5015b8e19bb70ac6d5342e7e51
christianjensen2903 <cmj290300@gmail.com>
2025-12-01 09:41:39 +0100

    🧹 Remove unnessecary exports

--- models/__init__.py
+++ models/__init__.py
@@ -1,18 +1,4 @@
 from .hetero_gnn import HeteroGNN  # type: ignore
-from .homo_gnn import DualEncoderGNN, CaseMetadataEncoder
-from preprocessing.graph_builder import (
-    LANGUAGE_VOCAB,
-    LANGUAGE_TO_IDX,
-    NUM_LANGUAGES,
-    encode_language,
-)
+from .homo_gnn import DualEncoderGNN
 
-__all__ = [
-    "HeteroGNN",
-    "DualEncoderGNN",
-    "CaseMetadataEncoder",
-    "LANGUAGE_VOCAB",
-    "LANGUAGE_TO_IDX",
-    "NUM_LANGUAGES",
-    "encode_language",
-]
+__all__ = ["HeteroGNN", "DualEncoderGNN"]
